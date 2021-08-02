4032aa05c19196326599dcc1a1389eaaf10c8049
Mattheus Del Vianple <kreator.esper@gmail.com>
2021-08-02 20:48:53 -0300

    JPD 0.1.0
    Why: Acompanhamento da equipe da produção do front-end do site-aplicação;
    
    How: Implementação de código (HTML e CSS)
    
    Tags: HTML, CSS, front, site
    
    Subjects
    ---------
    **Corrigida** duplicidade de pastas de site;
    **Adicionada** Seção Início;
    **Iniciada** Seção Sobre;

---
+++ site_JPD/js/script.js
@@ -0,0 +1,16 @@
+function trocarImgTeste(source) {
+
+    if(source == "sreg1"){
+        document.getElementById("img-teste").src= "imagens/pika.png"
+    }
+    if(source == "sreg2"){
+        document.getElementById("img-teste").src= "imagens/bagulho.png"
+    }
+
+    
+}
+
+function letMeCallYou()
+{
+    alert("Bazinga!!!  you called letMeCallYou")
+}
